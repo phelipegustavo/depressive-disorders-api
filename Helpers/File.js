f28444aeb364ac167e2c3e13dcfafdd6011e6119
Phelipe Gustavo <phelipediv@gmail.com>
2020-05-16 19:12:30 -0300

    initial commit

---
+++ Helpers/File.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+/**
+ * Read file from path
+ * 
+ * @param {String} path 
+ * @return {Promise}
+ */
+const readFile = (path) => {
+
+    return new Promise((res, rej) => {
+        const data = fs.readFileSync(path, "utf8")
+        if(data) {
+            return res(data)
+        }
+        return rej(false)
+    })
+}
+
+module.exports = {
+    readFile,
+}
